3f77dcc51385bcc255c3d3117393002f396bf15c
Saptadip Sarkar <informsapta@gmail.com>
2018-03-26 11:10:44 +0200

    Minor formatting change

--- mainLambdaFunction.min.py
+++ mainLambdaFunction.min.py
@@ -84,7 +84,7 @@ def handle_session_end_request():
 def get_welcome_response():
     session_attributes = {}
     card_title = "Crypto Genie"
-    speech_output = "Hello! I am Crypto Genie. I can help you to get all crypto currency related information very quickly. I will give you five choices. Tell me what you want me to do. Your options are: Choice 1:  Top crypto currency prices. Choice 2:  Ongoing I C O . Choice 3:  Social Media facts. Choice 4:  My portfolio. Choice 5:  Crypto headlines. "
+    speech_output = "Hello! I am Crypto Genie. I can help you to get all crypto currency related information very quickly. I will give you five choices. Tell me what you want me to do by selecting the choice number. Your options are: Choice 1:  Top crypto currency prices. Choice 2:  Ongoing I C O . Choice 3:  Social Media facts. Choice 4:  My portfolio. Choice 5:  Crypto headlines. "
     reprompt_text = "Please choose any option between one to five"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
@@ -103,7 +103,7 @@ def get_crypto_price():
         cur_price = str(round(float(j[count]['price_usd']), 2))
         speech_output += "Rank " + cur_rank + " : " + cur_name + ". Price : " + cur_price + " dollar " + ". ";
     session_attributes["userPromptedFor_getCryptoPrice"] = "true"
-    speech_output += "So I hope that I successfully fulfilled your request! Let's go for a coffee. Do you want me to serve you another request? I will do it free for you!! If you like then say yes, if not, then say no."
+    speech_output += "So I hope that I successfully fulfilled your request! Do you want me to serve you another request? I will do it free for you!! If you like then say yes. If you want to exit, then say no."
     reprompt_text = "I am still waiting for your response. Please say yes if you want to continue. Please say no if you want to exit."
     return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 def get_ico_info():
@@ -125,7 +125,7 @@ def get_ico_info():
         fmtd_ico_end_dt, fmtd_ico_end_tm = date_formatter(ico_end)
         speech_output += "I C O name: " + ico_name + ". Start date: " + fmtd_ico_strt_dt + ". Start time: " + fmtd_ico_strt_tm + ". End date: " + fmtd_ico_end_dt + ". End time: " + fmtd_ico_end_tm + ". Description: " + ico_desc + ". ";
     session_attributes["userPromptedFor_getIcoInfo"] = "true"
-    speech_output += "So that's all I have at the moment. Do you want me to do anything else? Please say yes to continue or no to exit."
+    speech_output += "So that's all I have at the moment. Do you want me to do anything else? Please say yes to continue or say no to exit."
     reprompt_text = "Is there something else that I can do for you ? If so, then say yes. If not, then say no. To exit, please say stop or cancel"
     return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 def get_quick_facts(intent, event):
@@ -228,7 +228,7 @@ def collect_social_media_info(intent):
             except KeyError:
                 fb_link = "not available"
             session_attributes["userPromptedFor_getQuickFacts"] = "true"
-            speech_output = " OK. Getting the latest social media related activities on " + str(currency_name) + ". Report is ready. Here we go: Twitter account statistics. Account name: " + str(twtr_acc_name) + ". Number of followers: " + str(twtr_follower_count) + ". Total tweet count: " + str(twtr_tweet_count) + ". Total number of tweets liked by the users: " + twtr_like_count + ". Reddit account statistics. Account name: " + str(rdit_acc_name) + ". Number of active users: " + str(rdit_actv_user_count) + ". Total number of subscribers: " + str(rdit_subscrb_count) + ". Number of posts per hour: " + str(rdit_posts_per_hour) + ". Number of comments per hour: " + str(rdit_comnts_per_hour) + ". Number of posts per day: " + str(rdit_posts_per_day) + ". Number of comments per day: " + str(rdit_comnts_per_day) + ". Facebook account statistics. Number of likes: " + str(fb_like_count) + ". Number of people talking about " + str(currency_name) + " on facebook is: " + str(fb_talking_count) + ". So that is all I can find on social media about " + str(currency_name) + ". I hope my report was useful for you.  Do you want me to do anything else for you? Please say yes or no."
+            speech_output = " OK. Getting the latest social media related activities on " + str(currency_name) + ". Report is ready. Here we go: Twitter account statistics. Account name: " + str(twtr_acc_name) + ". Number of followers: " + str(twtr_follower_count) + ". Total tweet count: " + str(twtr_tweet_count) + ". Total number of tweets liked by the users: " + twtr_like_count + ". Reddit account statistics. Account name: " + str(rdit_acc_name) + ". Number of active users: " + str(rdit_actv_user_count) + ". Total number of subscribers: " + str(rdit_subscrb_count) + ". Number of posts per hour: " + str(rdit_posts_per_hour) + ". Number of comments per hour: " + str(rdit_comnts_per_hour) + ". Number of posts per day: " + str(rdit_posts_per_day) + ". Number of comments per day: " + str(rdit_comnts_per_day) + ". Facebook account statistics. Number of likes: " + str(fb_like_count) + ". Number of people talking about " + str(currency_name) + " on facebook is: " + str(fb_talking_count) + ". So that is all I can find on social media about " + str(currency_name) + ". I hope my report was useful for you.  Do you want me to do anything else for you? Please say yes to continue. Or say no to exit."
             reprompt_text = "Hmm I did not get that. Do you want me to continue? Please say yes or no. To exit, please say stop or cancel"
     return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 def get_latest_news():
@@ -251,7 +251,7 @@ def get_latest_news():
         else:
             break
     session_attributes["userPromptedFor_getLatestNews"] = "true"
-    speech_output += "That's all for now. Do you want me to serve another request? I will do it for free!! If you like then say yes, if not, then say no.";
+    speech_output += "That's all for now. Do you want me to serve another request? I will do it for free!! If you like to continue then say yes. If not, then say no.";
     reprompt_text = "I am still waiting for your response. Please say yes if you want to continue. Please say no if you want to exit."
     return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 def get_portfolio():
